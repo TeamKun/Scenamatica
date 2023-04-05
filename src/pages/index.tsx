f889ad495d5b0ce2ff8849ec91343bb284a5f5d3
Peyang <peyang@peya.tokyo>
2023-04-05 19:33:56 +0900

    fix(docs): Unresolved files

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,6 @@ import clsx from "clsx"
 import Link from "@docusaurus/Link"
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext"
 import Layout from "@theme/Layout"
-import Features from "@site/src/components/Features"
 
 import styles from "./index.module.css"
 
