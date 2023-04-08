b6e13a0417e094b04bb27452193e713a33cd8e1c
Peyang <peyang@peya.tokyo>
2023-04-08 19:17:03 +0900

    docs: Writing your first scenario

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -180,7 +180,7 @@ const config = {
         prism: {
           theme: lightCodeTheme,
           darkTheme: darkCodeTheme,
-          additionalLanguages: ["java"],
+          additionalLanguages: ["java", "yaml"],
         },
         zoom: {
           background: {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,28 +6,21 @@ import Layout from "@theme/Layout"
 
 import styles from "./index.module.css"
 
-function Header() {
-    const { siteConfig } = useDocusaurusContext()
-    return (
-        <header className={clsx("hero hero--primary", styles.heroBanner)}>
-            <div className="container">
-                <h1 className="hero__title">{siteConfig.title}</h1>
-                <p className="hero__subtitle">{siteConfig.tagline}</p>
-                <div className={styles.buttons}>
-                    <Link className="button button--secondary button--lg" to="/docs/home">
-                        Scenamatica を使ってみる &gt;
-                    </Link>
-                </div>
-            </div>
-        </header>
-    )
-}
-
 export default function Home(): JSX.Element {
     const { siteConfig } = useDocusaurusContext()
     return (
         <Layout title={siteConfig.title} description="Scenamatica - 世界初の PaperMC プラグインのシナリオテスト自動化ツール">
-            <Header />
+            <header className={clsx("hero hero--primary", styles.heroBanner)}>
+                <div className="container">
+                    <h1 className="hero__title">{siteConfig.title}</h1>
+                    <p className="hero__subtitle">{siteConfig.tagline}</p>
+                    <div className={styles.buttons}>
+                        <Link className="button button--secondary button--lg" to="/docs/home">
+                            Scenamatica を使ってみる &gt;
+                        </Link>
+                    </div>
+                </div>
+            </header>
             <main>
                 <div className={styles.container}>
                     <h2>概要</h2>
