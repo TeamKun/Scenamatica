fbb29c2b8a9b250803c4bfb13924735592549ca1
PeyaPeyaPeyang <peyang@peya.tokyo>
2023-08-01 23:45:41 +0900

    chore: Add action templates

---
+++ .idea/fileTemplates/PlayerAction.java
@@ -0,0 +1,88 @@
+package org.kunlab.scenamatica.action.actions.player;
+
+import lombok.EqualsAndHashCode;
+import lombok.Value;
+import org.kunlab.scenamatica.action.actions.AbstractAction;
+import org.kunlab.scenamatica.interfaces.action.ActionArgument;
+import org.kunlab.scenamatica.interfaces.scenario.ScenarioEngine;
+import org.kunlab.scenamatica.interfaces.scenariofile.BeanSerializer;
+import org.kunlab.scenamatica.interfaces.scenariofile.trigger.TriggerArgument;
+import org.bukkit.event.Event;
+import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+public class ${NAME} extends AbstractPlayerAction<${NAME}.Argument>
+{
+    public static final String KEY_ACTION_NAME = "player_${PLAYER_ACTION_SERIALIZED_NAME}";
+    
+    @Override
+    public String getName()
+    {
+        return KEY_ACTION_NAME;
+    }
+
+    @Override
+    public void execute(@NotNull ScenarioEngine engine, @Nullable Argument argument)
+    {
+        argument = this.requireArgsNonNull(argument);
+    }
+
+    @Override
+    public boolean isFired(@NotNull Argument argument, @NotNull ScenarioEngine engine, @NotNull Event event)
+    {
+        if (!super.isFired(argument, engine, event))
+            return false;
+    }
+
+    @Override
+    public List<Class<? extends Event>> getAttachingEvents()
+    {
+        return Collections.singletonList(
+            
+        );
+    }
+
+    @Override
+    public Argument deserializeArgument(@NotNull Map<String, Object> map, @NotNull BeanSerializer serializer)
+    {
+    
+        return new Argument(
+            super.deserializeTarget(map),
+            
+        );
+    }
+ 
+    @Value
+    @EqualsAndHashCode(callSuper = true)
+    public static class Argument extends AbstractPlayerActionArgument
+    {
+       
+        public Argument(@NotNull String target, )
+        {
+            super(target);
+        }
+    
+        @Override
+        public boolean isSame(TriggerArgument argument)
+        {
+            if (!(argument instanceof Argument))
+                return false;
+
+            Argument arg = (Argument) argument;
+
+        }
+        
+        @Override
+        public String getArgumentString()
+        {
+            return appendArgumentString(
+                    super.getArgumentString(),
+            );
+        }
+    }
+}
