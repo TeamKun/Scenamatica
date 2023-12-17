6abd31bf69eb724f7c18255deb07dcbb1a0e7cf9
PeyaPeyaPeyang <peyang@peya.tokyo>
2023-12-18 04:20:14 +0900

    feat(actions): Name predicate

--- ScenamaticaActionEngine/src/main/java/org/kunlab/scenamatica/action/selector/compiler/SelectorCompiler.java
+++ ScenamaticaActionEngine/src/main/java/org/kunlab/scenamatica/action/selector/compiler/SelectorCompiler.java
@@ -9,6 +9,7 @@ import org.kunlab.scenamatica.action.selector.compiler.parser.SelectorSyntaxAnal
 import org.kunlab.scenamatica.action.selector.compiler.parser.SyntaxTree;
 import org.kunlab.scenamatica.action.selector.compiler.parser.SyntaxTreeTraverser;
 import org.kunlab.scenamatica.action.selector.predicates.LocationPredicate;
+import org.kunlab.scenamatica.action.selector.predicates.NamePredicate;
 import org.kunlab.scenamatica.action.selector.predicates.SelectorPredicate;
 
 import java.util.ArrayList;
@@ -40,6 +41,7 @@ public class SelectorCompiler
         ArrayList<SelectorPredicate<? super Entity>> predicates = new ArrayList<>();
         // <editor-fold desc="Predicates registering">
         predicates.add(new LocationPredicate());
+        predicates.add(new NamePredicate());
         // </editor-fold>
         return predicates;
     }
---
+++ ScenamaticaActionEngine/src/main/java/org/kunlab/scenamatica/action/selector/predicates/NamePredicate.java
@@ -0,0 +1,42 @@
+package org.kunlab.scenamatica.action.selector.predicates;
+
+import org.bukkit.entity.Entity;
+import org.kunlab.scenamatica.action.selector.compiler.AmbiguousString;
+
+import java.util.Map;
+
+public class NamePredicate extends AbstractGeneralEntitySelectorPredicate
+{
+    public static final String KEY_NAME = "name";
+
+    @Override
+    public boolean test(Entity entity, Map<? super String, Object> properties)
+    {
+        String nameDef = (String) properties.get(KEY_NAME);
+        if (nameDef == null)
+            return true;
+
+        String name = entity.getName();
+        AmbiguousString ambiguousString = AmbiguousString.fromMap(KEY_NAME, properties);
+
+        return ambiguousString.test(name);
+    }
+
+    @Override
+    public void normalizeMap(Map<? super String, Object> properties)
+    {
+        AmbiguousString.normalizeMap(KEY_NAME, KEY_NAME, properties);
+    }
+
+    @Override
+    public String[] getUsingAmbiguousStringKeys()
+    {
+        return new String[]{KEY_NAME};
+    }
+
+    @Override
+    public String[] getUsingKeys()
+    {
+        return new String[]{KEY_NAME};
+    }
+}
