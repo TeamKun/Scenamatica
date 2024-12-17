923598804504876b48c392246bfc9e7c848b4854
PeyaPeyaPeyang <peyang@peya.tokyo>
2024-12-17 10:21:06 +0900

    refactor(actions): Avoiding long parameters

--- Scenamatica/ScenamaticaStructures/src/main/java/org/kunlab/scenamatica/structures/minecraft/entity/entities/LightningStrikeStructureImpl.java
+++ Scenamatica/ScenamaticaStructures/src/main/java/org/kunlab/scenamatica/structures/minecraft/entity/entities/LightningStrikeStructureImpl.java
@@ -4,26 +4,20 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.bukkit.entity.Entity;
-import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LightningStrike;
-import org.bukkit.util.Vector;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.kunlab.scenamatica.enums.YAMLNodeType;
 import org.kunlab.scenamatica.exceptions.scenariofile.YamlParsingException;
 import org.kunlab.scenamatica.interfaces.scenariofile.StructureSerializer;
 import org.kunlab.scenamatica.interfaces.scenariofile.StructuredYamlNode;
-import org.kunlab.scenamatica.interfaces.structures.minecraft.entity.DamageStructure;
 import org.kunlab.scenamatica.interfaces.structures.minecraft.entity.EntityStructure;
 import org.kunlab.scenamatica.interfaces.structures.minecraft.entity.entities.LightningStrikeStructure;
-import org.kunlab.scenamatica.interfaces.structures.minecraft.misc.LocationStructure;
 import org.kunlab.scenamatica.nms.NMSProvider;
 import org.kunlab.scenamatica.nms.types.entity.NMSLightningStrike;
 import org.kunlab.scenamatica.structures.minecraft.entity.EntityStructureImpl;
 
-import java.util.List;
 import java.util.Map;
-import java.util.UUID;
 
 @Data
 @AllArgsConstructor
@@ -36,27 +30,6 @@ public class LightningStrikeStructureImpl extends EntityStructureImpl implements
     {
         super(entityStructure);
         this.effect = isEffect;
-    }
-
-    public LightningStrikeStructureImpl(@Nullable EntityType type, @NotNull EntityStructure original, Boolean effect)
-    {
-        super(type, original);
-        this.effect = effect;
-    }
-
-    public LightningStrikeStructureImpl(EntityType type, LocationStructure location, Vector velocity, String customName,
-                                        UUID uuid, Boolean glowing, Boolean gravity, Boolean silent,
-                                        Boolean customNameVisible, Boolean invulnerable, @NotNull List<String> tags,
-                                        Integer maxHealth, Integer health, DamageStructure lastDamageCause,
-                                        Integer fireTicks, Integer ticksLived, Integer portalCooldown,
-                                        Boolean persistent, Float fallDistance, Boolean effect)
-    {
-        super(
-                type, location, velocity, customName, uuid, glowing, gravity, silent, customNameVisible, invulnerable,
-                tags, maxHealth, health, lastDamageCause, fireTicks, ticksLived, portalCooldown, persistent,
-                fallDistance
-        );
-        this.effect = effect;
     }
 
     public LightningStrikeStructureImpl()
